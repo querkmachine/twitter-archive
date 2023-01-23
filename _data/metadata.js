fa53bb9ea8211b8a39a0445ca55cf8838c948810
Kimberly Grey <hi+github@berly.kim>
2023-01-23 22:40:19 +0000

    Initial data dump

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "batbeeps", // No leading @ here
+	homeLabel: "beeps.website",
+	homeUrl: "https://beeps.website/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
